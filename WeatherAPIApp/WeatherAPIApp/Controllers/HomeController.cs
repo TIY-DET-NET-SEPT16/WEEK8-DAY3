81c6f7d7080626dcc7ccec2b14f5cf1654925754
Ronda Bergman <rondabergman@hotmail.com>
2016-11-17 14:14:58 -0500

    daily

---
+++ WeatherAPIApp/WeatherAPIApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherAPIApp.Models;
+
+namespace WeatherAPIApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Weather2");
+        }
+
+        public JsonResult GetWeather()
+        {
+            WeatherClass myWeather = new WeatherClass();
+
+            return Json(myWeather.GetCurrentWeather(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WeatherAPIApp/WeatherAPIApp/Models/WeatherClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherAPIApp.Models
+{
+    public class WeatherClass
+    {
+        public object GetCurrentWeather()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Detroit&APPID=23b788bee2f442d46daf6f38eb174d06&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<object>(content);
+
+            return jsonContent;
+        }
+    }
+}
